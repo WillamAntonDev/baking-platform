814c08e944838daceda76e968ed4d7d7018a84f2
Banton65 <banton65@gmail.com>
2025-01-03 13:58:37 -0500

    Today's progress on HollysSweetStart

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from.models import Recipe
+from .models import Recipe
 
-
-admin.site.register(Recipe)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe
+from .forms import RecipeForm
 
-# Create your views here.
+def recipe_create(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("recipes:recipe_list")
+    else:
+        form = RecipeForm()
+    return render(request, "recipes/recipe_form.html", {"form": form})
+
+def recipe_edit(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect("recipes:recipe_detail", id=recipe.id)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, "recipes/recipe_form.html", {"form": form})
+
+def recipe_list(request):
+    # Fetch all recipes from the database
+    recipes = Recipe.objects.all()
+    return render(request, "recipes/recipe_list.html", {"recipes": recipes})
+
+def recipe_detail(request, id):
+    # Fetch a single recipe from the database
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, "recipes/recipe_detail.html", {"recipe": recipe})
+
+def recipe_delete(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    recipe.delete()
+    return redirect("recipes:recipe_list")
